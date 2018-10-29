cad620b33625a78b18224fe28aa6c7a3696255dd
Daniel Gustavsson <daniel.gustavsson@vntrs.com>
2018-10-29 20:19:17 +0100

    added basecode for frontend

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
+const path = require("path");
 
 const app = express();
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 app.get("/api/get-nearby-location", cors(), async (req, res, next) => {
   try {
